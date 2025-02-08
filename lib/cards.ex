98d2c45db6c8e10e82eabf269dfd343e76a42fc0
Shawn Page <shawn.page@gmail.com>
2025-02-08 15:27:09 -0500

    Added methods: deal, contains?, shuffle, create_deck.

--- lib/cards.ex
+++ lib/cards.ex
@@ -13,6 +13,46 @@ defmodule Cards do
 
   """
   def hello do
-    :world
+    # :world
+    "hi there"
   end
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits  = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+    #    for value <- values, suit <- suits do
+    #      "#{value} of #{suit}"
+    #    end
+
+
+#    cards = for suit <- suits do
+##      suit
+#      for value <- values do
+#        "#{value} of #{suit}"
+#      end
+##      "Is this a map"
+#    end
+##    List.flatten(cards)
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+#    Enum.any?(deck, fn x -> x == card end)
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
 end
